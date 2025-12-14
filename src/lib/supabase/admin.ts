b240abaac52004ed0a86e0641a178a7ce66ff0ba
H. S. Nam Bình <nahmsix2005@gmail.com>
2025-12-14 22:58:26 +0700

    fix: improve error handling

--- src/lib/supabase/admin.ts
+++ src/lib/supabase/admin.ts
@@ -1,10 +1,15 @@
 import { createClient } from '@supabase/supabase-js';
 
+// to prevent some accidental import in client-side bundles
+if (typeof window !== 'undefined') {
+  throw new Error('Admin client cannot be used on the client side!');
+}
+
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const serviceRoleKey = process.env.SUPABASE_SERVICE_ROLE_KEY!;
 
 if (!serviceRoleKey) {
-  throw new Error('SUPABASE_SERVICE_ROLE_KEY is not set.');
+  throw new Error('SUPABASE_SERVICE_ROLE_KEY is not set. This file must only be imported server-side.');
 }
 
 export function createAdminClient() {
